30271e16b2d0e772a9667c9cdf48abfd54f9f0f8
gappaljor-tgot <44777025+gappaljor-tgot@users.noreply.github.com>
2020-02-25 13:12:53 -0500

    export plugins

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var MultiSelectGroupPlugin = require('./src/plugins/multi-select-group.js');
 
 module.exports.HelloWorldPlugin = () => {console.log("Hello World");};
 module.exports.AnotherHelloWorldPlugin = () => {console.log("Hello World again");};
+
+module.exports.SelectGroupPlugin = SelectGroupPlugin;
+module.exports.MultiSelectGroupPlugin = MultiSelectGroupPlugin;
