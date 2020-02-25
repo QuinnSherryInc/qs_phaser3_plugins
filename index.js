cdd618f5a0bbf2ee6eb0f1823a6a437c0a2d4a95
gappaljor-tgot <44777025+gappaljor-tgot@users.noreply.github.com>
2020-02-25 13:05:38 -0500

    test

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-var SelectGroupPlugin = require('./src/plugins/select-group.js');
-var MultiSelectGroupPlugin = require('./src/plugins/multi-select-group.js');
-
 module.exports.HelloWorldPlugin = () => {console.log("Hello World");};
 module.exports.AnotherHelloWorldPlugin = () => {console.log("Hello World again");};
-
-module.exports.SelectGroupPlugin = SelectGroupPlugin;
-module.exports.MultiSelectGroupPlugin = MultiSelectGroupPlugin;
