c7f1f3e79ee959f47e1098239b99759527b19a60
gappaljor-tgot <44777025+gappaljor-tgot@users.noreply.github.com>
2020-02-25 10:11:29 -0500

    removed global scope

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ module.exports.AnotherHelloWorldPlugin = () => {console.log("Hello World again")
 
 module.exports.SelectGroupPlugin = SelectGroupPlugin;
 module.exports.MultiSelectGroupPlugin = MultiSelectGroupPlugin;
-
-global.HelloWorldPlugin = HelloWorldPlugin;
-global.AnotherHelloWorldPlugin = AnotherHelloWorldPlugin;
-global.SelectGroupPlugin = SelectGroupPlugin;
-global.MultiSelectGroupPlugin = MultiSelectGroupPlugin;
